0c6391ded54319deb1999e0ad6a1fa17239afc34
ilhamyon <ilhamyon@gmail.com>
2024-08-15 13:07:28 +0700

    adjust current location

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -149,6 +149,7 @@ function Home() {
         (position) => {
           const { latitude, longitude } = position.coords;
           setPosition({ lat: latitude, lng: longitude });
+          setGeometry({ lat: latitude, lng: longitude });
           message.success('Berhasil menyimpan titik lokasi');
         },
         (error) => {
